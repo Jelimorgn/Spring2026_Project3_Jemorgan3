ecc2b168b7590a8704f71835d37d6ab3c78a9227
Jemorgan3 <Jemorgan3@crimson.ua.edu>
2026-03-31 18:40:31 -0500

    Add project files.

---
+++ Spring2026_Project3_Jemorgan3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project3_Jemorgan3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
